16aabcb994d2e872fd5f2f880da003ddea070667
Wesley Maciel <weslmaciel@gmail.com>
2025-07-17 10:36:03 -0300

    Running project ind headless mode

--- src/test/java/org/example/seleniumprojecttest/core/DriveManager.java
+++ src/test/java/org/example/seleniumprojecttest/core/DriveManager.java
@@ -3,13 +3,17 @@ package org.example.seleniumprojecttest.core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriveManager {
     private static WebDriver driver;
     public static WebDriver getDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
